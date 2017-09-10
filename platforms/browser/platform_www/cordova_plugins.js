dfc2d3b1318285fc5f82730c05822c000a1875af
lostndessence <30732892+lostndessence@users.noreply.github.com>
2017-09-10 10:02:42 -0500

    testing

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -1,15 +1,35 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/cordova-sqlite-plugin/www/sqlite.js",
-        "id": "cordova-sqlite-plugin.SQLite",
-        "pluginId": "cordova-sqlite-plugin"
+        "file": "plugins/cordova-sqlite-storage/www/SQLitePlugin.js",
+        "id": "cordova-sqlite-storage.SQLitePlugin",
+        "pluginId": "cordova-sqlite-storage",
+        "clobbers": [
+            "SQLitePlugin"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+        "id": "cordova-plugin-dialogs.notification",
+        "pluginId": "cordova-plugin-dialogs",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/browser/notification.js",
+        "id": "cordova-plugin-dialogs.notification_browser",
+        "pluginId": "cordova-plugin-dialogs",
+        "merges": [
+            "navigator.notification"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-sqlite-plugin": "3.1.1"
+    "cordova-sqlite-storage": "2.0.4",
+    "cordova-plugin-dialogs": "1.3.3"
 }
 // BOTTOM OF METADATA
 });
--- platforms/browser/www/js/db.js
+++ platforms/browser/www/js/db.js
@@ -1,41 +1,17 @@
-function runSQLTest(){
-//start sqllite
-var myDB = window.sqlitePlugin.openDatabase({name: "mySQLite.db", location: 'default'});
-
-//open DB
-myDB.transaction(function(transaction) {
-transaction.executeSql('CREATE TABLE IF NOT EXISTS phonegap_pro (id integer primary key, title text, desc text)', [],
-	function(tx, result) {
-		alert("Table created successfully");
-	},
-	function(error) {
-		alert("Error occurred while creating the table.");
-	});
+var db = null;
+ 
+document.addEventListener('deviceready', function() {
+  db = window.sqlitePlugin.openDatabase({name: 'demo.db', location: 'default'});
+  console.log(navigator.notification);
+  
+  db.transaction(function(tx) {
+    tx.executeSql('CREATE TABLE IF NOT EXISTS DemoTable (name, score)');
+    tx.executeSql('INSERT INTO DemoTable VALUES (?,?)', ['Alice', 101]);
+    tx.executeSql('INSERT INTO DemoTable VALUES (?,?)', ['Betty', 202]);
+  }, function(error) {
+    console.log('Transaction ERROR: ' + error.message);
+  }, function() {
+    navigator.notification.alert('Populated database OK',alertDismissed,'Message', 'Clear');
+  });
+  
 });
-
-//Add test values to DB
-var title="sundaravel";
-var desc="phonegap freelancer";
-myDB.transaction(function(transaction) {
-var executeQuery = "INSERT INTO phonegap_pro (title, desc) VALUES (?,?)";
-transaction.executeSql(executeQuery, [title,desc]
-, function(tx, result) {
-alert('Inserted');
-},
-function(error){
-alert('Error occurred');
-});
-});
-
-//View Data
-myDB.transaction(function(transaction) {
-transaction.executeSql('SELECT * FROM phonegap_pro', [], function (tx, results) {
-var len = results.rows.length, i;
-$("#rowCount").append(len);
-for (i = 0; i < len; i++){
-$("#TableData").append("<tr><td>"+results.rows.item(i).id+"</td><td>"+results.rows.item(i).title+"</td><td>"+results.rows.item(i).desc+"</td></tr>");
-}
-}, null);
-});
-
-}
