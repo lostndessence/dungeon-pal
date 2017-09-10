dae0af997c653dcc952bca52f8ff266adc2d6559
lostndessence <30732892+lostndessence@users.noreply.github.com>
2017-09-10 14:55:45 -0500

    working db

--- www/js/db.js
+++ www/js/db.js
@@ -3,15 +3,30 @@ var db = null;
 document.addEventListener('deviceready', function() {
   db = window.openDatabase("test", "1.0", "Test DB", 1000000);
   
+  //create database
   db.transaction(function(tx) {
-    tx.executeSql('CREATE TABLE IF NOT EXISTS DemoTable (name, score)');
-    tx.executeSql('INSERT INTO DemoTable VALUES (?,?)', ['Alice', 101]);
-    tx.executeSql('INSERT INTO DemoTable VALUES (?,?)', ['Betty', 202]);
+    tx.executeSql('CREATE TABLE IF NOT EXISTS PCs (id INTEGER, charname TEXT)');
   }, function(error) {
     console.log('Transaction ERROR: ' + error.message);
   }, function() {
     console.log('Populated database OK');
   });
+
+   db.transaction(function(tx) {
+		var html = '<table><thead><th>ID</th><th>Player Character Name </th></thead><tbody>';
+                tx.executeSql('SELECT * FROM PCs', [], function (tx, data) {
+                    for (i = 0; i < data.rows.length; i++) {
+                        html += '<tr><td>' + data.rows[i].id + '</td><td>' + data.rows[i].charname + '</td></tr>';
+                    };
+                    html += '</tbody></table>';
+                    $('#TableData').html(html);
+                });
+	}, function(error) {
+    console.log('Transaction ERROR: ' + error.message);
+  }, function() {
+    console.log('Displayed database OK');
+  });
+  
   
 });
 
